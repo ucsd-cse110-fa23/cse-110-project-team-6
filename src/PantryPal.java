88f8c59915c509e03984220f7f7640b2237bf6b9
Starnaphie <stephanie.xu22@gmail.com>
2023-11-04 15:15:50 -0700

    Co-authored-by: ken004ucsd <ken004ucsd@users.noreply.github.com>

--- src/PantryPal.java
+++ src/PantryPal.java
@@ -20,42 +20,57 @@ import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter; 
 
+class Consts {
+    public final Color GREEN = Color.web("#A6D69B");
+    public final Color YELLOW = Color.web("#F1E293");
+    public final Color LIGHT = Color.web("F1EBE2");
+    public final Color DARK = Color.web("17373A");
+
+    public final int WIDTH = 750;
+    public final int HEIGHT = 1000;
+    public final int RECIPE_OFFSET = 5;
+    public final int HF_HEIGHT = 80;
+}
+
 class RecipeUnitView extends StackPane {
+    Consts consts = new Consts();
+    
     Recipe recipe;
     Rectangle background;
     Button detailView;
     Text recipeName;
-
+    
+    private final int RECIPE_WIDTH = 600;
+    private final int RECIPE_HEIGHT = 80;
+    
     RecipeUnitView (Recipe recipe) {
         this.recipe = recipe;
 
-        this.setWidth(700);
-        this.setHeight(80);
+        this.setWidth(RECIPE_WIDTH);
+        this.setHeight(RECIPE_HEIGHT);
         this.setAlignment(Pos.CENTER_LEFT);
 
         // round rectangle
         background = new Rectangle();
-        background.setWidth(700);
-        background.setHeight(80);
-        background.setArcHeight(30);
-        background.setArcWidth(30);
+        background.setWidth(RECIPE_WIDTH);
+        background.setHeight(RECIPE_HEIGHT);
+        background.setArcHeight(35);
+        background.setArcWidth(35);
 
         //style
-        background.setFill(Color.web("#cee6ba"));
-        background.setStroke(Color.web("#ccbc97"));
-        background.setStrokeWidth(5);
+        background.setFill(consts.YELLOW);
         this.getChildren().add(background);
 
         // text
         recipeName = new Text(recipe.getName());
         this.setMargin(recipeName, new Insets(0, 0, 0, 20));        // top, right, bottom, left
-        recipeName.setStyle("-fx-font-color: #2e2e2e; -fx-border-width: 0; -fx-font-weight: bold; -fx-font-size: 30");
+        recipeName.setStyle("-fx-border-width: 0; -fx-font-size: 30");
         this.getChildren().add(recipeName);
 
         // invisible button
         detailView = new Button();
         detailView.setStyle("-fx-background-color: transparent");
-        detailView.setPrefSize(700, 80);
+        detailView.setPrefSize(RECIPE_WIDTH, RECIPE_HEIGHT);
         this.getChildren().add(detailView);
         
         addListeners();
@@ -71,21 +86,30 @@ class RecipeUnitView extends StackPane {
 }
 
 class RecipeListView extends VBox {
-    List<Recipe> recipes;
+    Consts consts = new Consts();
+
+    List<Recipe> recipes = new ArrayList<>();
 
     RecipeListView(Stage primaryStage) {
-        this.setWidth(750);
+        this.setWidth(consts.WIDTH);
         this.setPrefHeight(840);
-        this.setStyle("-fx-background-color: #defce6; -fx-border-width: 0; -fx-font-weight: bold;");
+        this.setSpacing(consts.RECIPE_OFFSET);
+        this.setBackground(new Background(new BackgroundFill(consts.LIGHT, CornerRadii.EMPTY, Insets.EMPTY)));
         this.setAlignment(Pos.TOP_CENTER);
 
         Recipe test = new Recipe("red wine potatoes", "2", "69", "potatoes, wine", "bake, eat");
 
-        RecipeUnitView ex = new RecipeUnitView(test);
-        this.setMargin(ex, new Insets(5, 0, 0, 20));
+        recipes.add(test);
 
+        renderRecipes();
+    }
 
-        this.getChildren().add(ex);
+    private void renderRecipes() {
+        for (int i = 0; i < 10; i++) {
+            this.getChildren().add(new RecipeUnitView(recipes.get(0)));
+            this.setMargin(this.getChildren().get(i), new Insets(0, 0, 0, 75));
+        }
+        this.setMargin(this.getChildren().get(0), new Insets(5, 0, 0, 75));
     }
 }
 
@@ -105,17 +129,21 @@ class RecipeFullView extends BorderPane{
 }
 
 class Footer extends HBox {
+    Consts consts = new Consts();
+    
     public Button newRecipe;
     
     Footer(Stage primaryStage) {
-        this.setPrefSize(750, 80);
+        this.setPrefSize(consts.WIDTH, consts.HF_HEIGHT);
+        this.setBackground(new Background(new BackgroundFill(consts.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+        this.setAlignment(Pos.CENTER_RIGHT);
 
         newRecipe = new Button("New Recipe");
         newRecipe.setPrefSize(100, 40);
-        newRecipe.setStyle("-fx-background-color: #e3e3e3; -fx-border-width: 0; -fx-font-weight: bold; -fx-font-color: #2e2e2e"); // set background color of texfield
-
-        this.setMargin(newRecipe, new Insets(20, 20, 20, 20));
-        this.setAlignment(Pos.CENTER_RIGHT);
+        newRecipe.setBackground(new Background(new BackgroundFill(consts.YELLOW, CornerRadii.EMPTY, Insets.EMPTY)));
+        newRecipe.setTextFill(consts.DARK);
+        newRecipe.setStyle("-fx-border-width: 0"); 
+        this.setMargin(newRecipe, new Insets(20, 20, 20, 20));  
         this.getChildren().add(newRecipe);
 
         addListeners(primaryStage);
@@ -123,19 +151,95 @@ class Footer extends HBox {
 
     private void addListeners (Stage primaryStage) {
         newRecipe.setOnAction(e -> {
-            // fill in
+            // TO DO add button functionality
+            System.out.println("clicked add recipe");
         });
     }
 }
 
 class Header extends HBox { 
-    Header() {
-        this.setPrefSize(750, 80);
+    Consts consts = new Consts();
 
-        Text titleText = new Text("PantryPal"); // Text of the Header
-        titleText.setStyle("-fx-background-color: #e3e3e3; -fx-border-width: 0; -fx-font-weight: bold; -fx-font-size: 40; -fx-font-color: #2e2e2e");
-        this.getChildren().add(titleText);
+    Header(String heading) {
+        this.setPrefSize(consts.WIDTH, consts.HF_HEIGHT);
+        this.setBackground(new Background(new BackgroundFill(consts.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
         this.setAlignment(Pos.CENTER); // Align the text to the Center
+
+        Text titleText = new Text(heading); // Text of the Header
+        titleText.setFont(Font.font("Verdana", 40));
+        titleText.setFill(consts.DARK);
+        this.getChildren().add(titleText);
+    }
+}
+
+class MealDesign extends StackPane {
+    private Rectangle background;
+    private Text meal;
+
+    //temp button fuctionality
+    private Button mealButton;
+
+    MealDesign (String mealType) {
+        // rectangle
+        background = new Rectangle();
+        background.setWidth(400);
+        background.setHeight(80);
+        background.setArcHeight(35);
+        background.setArcWidth(35);
+
+        //style
+        background.setFill(Color.web("#cee6ba"));
+        background.setStroke(Color.web("#ccbc97"));
+        background.setStrokeWidth(5);
+        this.getChildren().add(background);
+
+        // meal type
+        meal = new Text(mealType);
+        this.getChildren().add(meal);
+
+        //invisible button
+        //TEMP FUNCTIONALYLITY    
+        mealButton = new Button();
+        mealButton.setStyle("-fx-background-color: transparent");
+        mealButton.setPrefSize(400, 80);
+        this.getChildren().add(mealButton);
+        
+        addListeners();
+    }
+
+        private void addListeners () {
+            mealButton.setOnAction(e -> {
+            // TO DO add button functionality
+            System.out.println("selected meal type");
+        });
+    }
+}
+
+class MealTypeOptions extends VBox {
+    Consts consts = new  Consts();
+
+    MealTypeOptions (Stage primaryStage){
+        this.setWidth(750);
+        this.setPrefHeight(840);
+        this.setSpacing(5);
+        this.setSpacing(5);
+        this.setBackground(new Background(new BackgroundFill(consts.LIGHT, CornerRadii.EMPTY, Insets.EMPTY)));
+        this.setAlignment(Pos.TOP_CENTER);
+        
+        MealDesign breakfast = new MealDesign("Breakfast");
+        MealDesign lunch = new MealDesign("Lunch");
+        MealDesign dinner = new MealDesign("Dinner");        
+        this.getChildren().add(breakfast);
+        this.getChildren().add(lunch);
+        this.getChildren().add(dinner);
+    }
+}
+
+class MealType extends BorderPane {
+    private Header header;
+
+    MealType (Stage primaryStage) {
+
     }
 }
 
@@ -147,7 +251,7 @@ class Homepage extends BorderPane {
     private Footer footer;
 
     Homepage (Stage primaryStage) {
-        header = new Header();
+        header = new Header("PantryPal");
         footer = new Footer(primaryStage);
         recipeList = new RecipeListView(primaryStage);
         // this.setStyle("-fx-background-color: #000000; -fx-border-width: 0; -fx-font-weight: bold;");
@@ -187,8 +291,8 @@ class AppFrame extends BorderPane{
  * The main class which extends the Application class and implements the start method to launch the mini-project app
  */
 public class PantryPal extends Application {
+    Consts consts = new Consts();
 
-    
     /*
      * The start method launches the mini-project window with all the respective features
      * 
@@ -203,7 +307,7 @@ public class PantryPal extends Application {
         // Set the title of the app
         primaryStage.setTitle("PantryPal");
         // Create scene of mentioned size with the border pane
-        primaryStage.setScene(new Scene(root, 750, 1000));
+        primaryStage.setScene(new Scene(root, consts.WIDTH, consts.HEIGHT));
         // Make window non-resizable
         primaryStage.setResizable(false);
         // Show the app
